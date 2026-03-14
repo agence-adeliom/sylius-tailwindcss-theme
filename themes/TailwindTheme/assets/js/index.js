4c32527653200cffecbf96ce4ee32bc5cb8c1bd7
Jérôme Engeln <jerome.engeln@adeliom.com>
2026-03-14 15:12:42 +0100

    feat: theme switcher
    Signed-off-by: Jérôme Engeln <jerome.engeln@adeliom.com>

--- themes/TailwindTheme/assets/js/index.js
+++ themes/TailwindTheme/assets/js/index.js
@@ -1,3 +1,12 @@
+import { SyliusToggleTheme, SyliusApplyTheme } from './sylius-toggle-theme';
 import '@fortawesome/fontawesome-free/css/solid.css';
 import '@fortawesome/fontawesome-free/scss/brands.scss';
 import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Toggle themes
+    SyliusToggleTheme();
+});
+
+// Apply theme
+SyliusApplyTheme();
